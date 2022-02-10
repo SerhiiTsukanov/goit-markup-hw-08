573fa28b836e342e1980784b6806a53f3a2f2e51
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-02-11 01:46:29 +0200

    header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,14 @@
 (() => {
-  const menuContainer = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const refs = {
+    openMenuBtn: document.querySelector("[data-mobile-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-mobile-menu-close]"),
+    menu: document.querySelector("[data-mobile-menu]"),
+  };
 
-  const toggleMenu = () => menuContainer.classList.toggle('is-open');
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window
-    .matchMedia('(min-width: 600px)')
-    .addEventListener(
-      'change',
-      e => e.matches && menuContainer.classList.remove('is-open'),
-    );
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
 })();
